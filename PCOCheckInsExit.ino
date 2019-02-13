511476c5f6487ce170acd3425123b7a863deb814
Bill Deitrick <bill@deitrick.us>
2019-02-13 10:35:55 -0500

    Initial commit

---
+++ PCOCheckInsExit.ino
@@ -0,0 +1,100 @@
+
+#define slowMode 0 // Set to 1 if Centipede appears to be moving too quickly at any screen. This will slow down the entire process
+
+// Special characters definition
+#define KEY_LEFT_CTRL  0x80
+#define KEY_LEFT_SHIFT 0x81
+#define KEY_LEFT_ALT   0x82
+#define KEY_RIGHT_CTRL 0x84
+#define KEY_RIGHT_SHIFT    0x85
+#define KEY_RIGHT_ALT  0x86
+#define KEY_UP_ARROW   0xDA
+#define KEY_DOWN_ARROW 0xD9
+#define KEY_LEFT_ARROW 0xD8
+#define KEY_RIGHT_ARROW    0xD7
+#define KEY_BACKSPACE  0xB2
+#define KEY_TAB        0xB3
+#define KEY_ENTER 0xB0
+#define KEY_ESC        0xB1
+#define KEY_CAPS_LOCK  0xC1
+
+int buttonPin = 2;  // Set a button to any pin
+int RXLED = 17;
+static uint8_t __clock_prescaler = (CLKPR & (_BV(CLKPS0) | _BV(CLKPS1) | _BV(CLKPS2) | _BV(CLKPS3)));
+
+void wait(int cycles = 1);
+
+void setup()
+{
+  setPrescaler(); // Set prescaler to highest clock speed
+  Keyboard.begin(); // Start they keyboard emulator
+  pinMode(buttonPin, INPUT);  // Set up the debugging pin. If you want to debug the code, use a length of wire to connect pins 2 and GND on the board
+  digitalWrite(buttonPin, HIGH);
+  pinMode(RXLED, OUTPUT); // Configure the on-board LED
+  digitalWrite(RXLED, LOW);
+  TXLED1;
+  wait(5); // Wait for all services to finish loading
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == 1 ) { // Check for debugging. If not debugging, run the program
+    TXLED1; // Toggle the TX on-board LED
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_LEFT_ALT);
+    Keyboard.press(KEY_ENTER);
+    Keyboard.release(KEY_LEFT_CTRL);
+    Keyboard.release(KEY_LEFT_ALT);
+    Keyboard.release(KEY_ENTER);
+    TXLED0;
+    wait(120);
+  }
+  bootLoop();
+}
+
+void bootLoop() {
+  //      digitalWrite(RXLED, LOW);   // set the LED on
+  TXLED0; //TX LED is not tied to a normally controlled pin
+  delay(200);              // wait for a second
+  TXLED1;
+  delay(200);
+  TXLED0; //TX LED is not tied to a normally controlled pin
+  delay(200);              // wait for a second
+  TXLED1;
+  delay(800);
+}
+
+void blink() {
+  digitalWrite(RXLED, LOW);
+//  TXLED1;
+  delay(250);
+  digitalWrite(RXLED, HIGH);
+//  TXLED0;
+  delay(250);
+}
+
+void wait(int cycles) {
+  for (int i = 0; i < cycles; i++) {
+    blink();
+    if (slowMode) {
+      delay(250);
+    }
+  }
+}
+
+void setPrescaler() {
+  // Disable interrupts.
+  uint8_t oldSREG = SREG;
+  cli();
+
+  // Enable change.
+  CLKPR = _BV(CLKPCE); // write the CLKPCE bit to one and all the other to zero
+
+  // Change clock division.
+  CLKPR = 0x0; // write the CLKPS0..3 bits while writing the CLKPE bit to zero
+
+  // Copy for fast access.
+  __clock_prescaler = 0x0;
+
+  // Recopy interrupt register.
+  SREG = oldSREG;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# PCO Check-ins Exit
+
+This is a simple Arduino script that turns an Arduino device into an exit "key" for [PCO](http://planning.center) Check-ins kiosks. Basically, this turns the device into a keyboard emulator that will simulate the CTRL + ALT + ENTER sequence when plugged into a USB port. This is useful for Windows devices without an external keyboard that have also had the on-screen keyboard disabled. Inspired (and some code shamelessly swiped from) the "[Centipede](https://labs.amplifiedit.com/centipede/)" project from Amplified Labs.
+
+Worked with an [Arduino Pro Micro](https://www.amazon.com/gp/product/B012FOV17O/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B012FOV17O&linkCode=as2&tag=ampit01-20&linkId=f088280a53c7896a06d4dc3479ae4526).
